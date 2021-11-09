f7748efa45fe3191422d029860327dea341e019e
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-11-09 10:20:21 -0500

    add file

---
+++ hw6-2.py
@@ -0,0 +1,21 @@
+# author :Lm (AMDG) 11/9/21
+# #Question 1
+my_lst = [0, 1, 2, 3, 4, 5, 6, 7,]
+# Question 2
+print(len(my_lst))
+# Question 3
+print(my_lst[3])
+# Question 4 
+print( 5 not in my_lst)
+# Question 5 
+print( 10 in my_lst)
+# Question 6
+newlst = ["Luis", "Ryan", "Mateo", "Jason"]
+# Question 7
+print(newlst[0])
+# Question 8
+print(newlst * 3)
+# Question 9
+print( my_lst + newlst)
+# Question 10
+print(len(my_lst + newlst))
